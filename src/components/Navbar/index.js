e063c9372966ceeca74b29711e42353d63c8b869
modakverma-tmpl <modak.v@tunica.tech>
2024-01-09 16:27:01 +0530

    search result display functionality

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,30 +1,78 @@
-import React,{useState} from 'react'
+import React,{useEffect, useState} from 'react'
 import { NAVBAR_ITEMS } from '../../models/NavbarModel'
 import NavbarItem from './NavbarItem';
 import Logo from '../Logo';
 import Input from '../../base-components/Input'
 import Button from '../../base-components/Button'
 import hamburgerIcon from '../../assets/hamburger-icon.svg'
+import { PROD_URL } from '../../utils/http'
+import useSearchData from '../../hooks/useSearchData'
+import Loader from '../Loader';
+import Error from '../Error';
+import DropDown from '../../base-components/DropDown';
+import { toast } from 'react-toastify';
 
 const Navbar = () => {
-  const [showMenu, setShowMenu] = useState(false)
+  const [showMenu, setShowMenu] = useState(false);
+  const [showDropDown, setSowDropDown] = useState(false);
+  const [keyword,setKeyword] = useState('myname')
   const handleMenuToggle = () => {
     setShowMenu(prev => !prev);
   }
+  const subUrl = `/search?keyword=${keyword}`
+  const {data,isLoading,isError,error,refetch} = useSearchData(PROD_URL+subUrl,'search')
+
+  let searchData = data?.data;
   const handleSearch = () => {
-    
+    if (keyword==='') {
+      toast.warn('Please enter something to search...')
+      return;
+    }
+    setSowDropDown(true)
+    refetch();
   }
+  if (isError) {
+    searchData = [{
+      id:'1',
+      title: 'nothing to show...'
+    }]
+    console.log(isError)
+    console.log(isLoading)
+  }
+
+  const handleKeywordChange = (event) => {
+    setKeyword(event.target.value)
+  }
+
+  const setStateCallback = (toggle) => {
+    console.log(toggle)
+    setSowDropDown(toggle)
+  }
+
   return (
     <div className="flex gap-4 w-full items-center justify-between px-4 sm:px-10 border-b h-24 relative">
       <Logo dest="navbar" />
 
       {/*=== SERRCH BAR ===*/}
-      <div className='hidden lg:flex'>
-        <Input
-          onChange={handleSearch}
-          placeholder="Enter here..."
-        />
-        <Button>Search</Button>
+      <div className='relative'>
+          <div className="hidden lg:flex">
+              <Input
+                value={keyword}
+                onChange={handleKeywordChange}
+                placeholder="Enter here..."
+              />
+          {isLoading ?
+            <Loader />:
+            <Button
+              onClick={handleSearch}
+            >Search</Button>
+          }
+            </div>
+        {searchData && showDropDown && <DropDown
+          className="hidden lg:block"
+          listItem={searchData}
+          setStateCallback={setStateCallback}
+        />}
       </div>
 
       <div className='hidden lg:flex gap-5'>
@@ -35,15 +83,33 @@ const Navbar = () => {
         <img
           onClick={handleMenuToggle}
           className="lg:hidden w-14 p-2  cursor-pointer hover:bg-slate-300/20 transition rounded-lg"
-          src={hamburgerIcon} alt="hamburger-icon-url" />
+        src={hamburgerIcon} alt="hamburger-icon-url" />
+      
+      {/* === MOBILE VIEW === */}
       {showMenu &&
       <div className='lg:hidden cursor-pointer flex flex-col absolute right-2 top-24 w-full sm:w-1/3 bg-primary items-center shadow-md rounded-lg p-2 gap-2 bg-white-300/20 font-sans '>
        { NAVBAR_ITEMS.map((item) => (
         <NavbarItem item={item} />)
         )}
-      <div className="flex">
-          <Input placeholder="Enter here..." />
-          <Button>Search</Button>
+      <div className='relative'>
+          <div className="flex">
+              <Input
+                value={keyword}
+                onChange={handleKeywordChange}
+                placeholder="Enter here..."
+              />
+              {isLoading ?
+              <Loader />:
+              <Button
+              onClick={handleSearch}
+              >Search</Button>
+              }
+            </div>
+            {searchData && showDropDown && <DropDown
+              className='right-0'
+              listItem={searchData}
+              setStateCallback={setStateCallback}
+            />}
       </div>
       </div>
       }
--- src/pages/Home.page.js
+++ src/pages/Home.page.js
@@ -7,7 +7,7 @@ import { PROD_URL } from '../utils/http';
 import { twMerge } from 'tailwind-merge';
 
 const Home = () => {
-    const perPage = 5;
+    const perPage = 6;
     const [currentPage, setCurrentPage] = useState(1);
     const [currentBanner, setCurrentBanner] = useState(null);
     const[bannerData,setBannerData] = useState(null)
@@ -48,7 +48,7 @@ const Home = () => {
                 <h1 className='font-black text-5xl text-center'>{bannerData.title}</h1>
                 <p className='w-3/5 text-center font-sans font-light text-xl tracking-wide'>{ bannerData.subtitle}</p>
             </div>}
-            <div className='border-t-[1.8px] w-[41rem] pt-10 border-black flex items-center justify-center w-full flex-col'>
+            <div className='border-t-[1.8px] w-[40rem] pt-10 border-black flex items-center justify-center w-full flex-col'>
             <h1 className='font-black text-3xl pb-6'>All Articles</h1>
             <div className='font-sans w-full grid grid-cols-1 md:grid-cols-2 gap-y-4'>
             {blogs?.map(blog => (
